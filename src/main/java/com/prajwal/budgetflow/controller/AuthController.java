b3fd14f853b6eb12f2826d8d8a88b97c2f8a5a0d
Prajwal Kasbale <kasbaleprajwal@gmail.com>
2026-06-27 02:57:30 +0530

    Configured Spring Security to allow authentication endpoints and protect all other APIs

---
+++ src/main/java/com/prajwal/budgetflow/controller/AuthController.java
@@ -0,0 +1,39 @@
+package com.prajwal.budgetflow.controller;
+
+import com.prajwal.budgetflow.dto.AuthRequest;
+import com.prajwal.budgetflow.dto.AuthResponse;
+import com.prajwal.budgetflow.dto.RegisterRequest;
+import com.prajwal.budgetflow.service.AuthService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<String> register(
+            @Valid @RequestBody RegisterRequest request) {
+
+        authService.register(request);
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body("User registered successfully.");
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login(
+            @Valid @RequestBody AuthRequest request) {
+
+        return ResponseEntity.ok(authService.login(request));
+    }
+}
