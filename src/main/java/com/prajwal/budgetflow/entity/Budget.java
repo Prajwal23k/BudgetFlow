0ec4ce765c87d8bd3dc48cbdc483c6f39aeeef1b
Prajwal Kasbale <kasbaleprajwal@gmail.com>
2026-06-29 08:22:15 +0530

    Added unique constraint for monthly budgets

--- src/main/java/com/prajwal/budgetflow/entity/Budget.java
+++ src/main/java/com/prajwal/budgetflow/entity/Budget.java
@@ -6,7 +6,20 @@ import lombok.*;
 import java.math.BigDecimal;
 
 @Entity
-@Table(name = "budgets")
+@Table(
+        name = "budgets",
+        uniqueConstraints = {
+                @UniqueConstraint(
+                        name = "uk_budget_user_category_month_year",
+                        columnNames = {
+                                "user_id",
+                                "category_id",
+                                "month",
+                                "year"
+                        }
+                )
+        }
+)
 @Getter
 @Setter
 @NoArgsConstructor
